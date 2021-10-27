5b6879d75162ed748046738780b2034e4324e403
Thammasorn Yimsawat <thammasorn.y@ku.th>
2021-10-27 17:42:37 +0700

    edit report models

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -9,12 +9,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Report extends Model
 {
     use HasFactory,SoftDeletes;
-
+    protected $appends = ['report_by'];
     public function assignment(){
         return $this->belongsTo(Assignment::class);
     }
 
     public function redBox(){
         return $this->belongsTo(RedBox::class);
+    }
+
+    public function getReportByAttribute(){
+        return  $this->assignment->patrol_name ;
     }
 }
